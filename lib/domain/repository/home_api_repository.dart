acf2f04197ce979c74334f96ec0420a95d772470
KyuminKim <hivedora@gmail.com>
2022-09-16 00:16:07 +0900

    [RESOLVE] fix geolocator to Location

--- lib/domain/repository/home_api_repository.dart
+++ lib/domain/repository/home_api_repository.dart
@@ -1,5 +1,7 @@
 import 'dart:developer';
 
+import 'package:location/location.dart';
+
 import '../../data/banner_api.dart';
 import '../../data/post_api.dart';
 import '../model/fitnesscenter.dart';
@@ -7,6 +9,7 @@ import '../model/post.dart';
 import '../model/banner.dart';
 import '../util.dart';
 import 'fitness_center_api_repository.dart';
+import '../instance_preference/location.dart';
 
 class HomeApiRepository {
   final postApi = PostApi();
@@ -16,28 +19,36 @@ class HomeApiRepository {
   Future<Map> getHomeRepo() async {
     List<Post> _posts = <Post>[];
     List<Banner> _banners = <Banner>[];
-
     _posts = await postApi.getPost();
     _banners = await bannerApi.getBanner();
 
-
-    Position _position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high);
-
+    Map _position = await locator.getAndSendLocation(null);
 
     //FitnessCenter _fitness = await fitnessCenterRepo.getFitnessRepo(1, _position.longitude, _position.latitude, _position.longitude, _position.latitude);
-    FitnessCenter _fitness =
-        await fitnessCenterRepo.getFitnessRepo(1, 100, 100, 100, 100);
+    // FitnessCenter _fitness =
+    //     await fitnessCenterRepo.getFitnessRepo(1, 100, 100, 100, 100);
 
-
-    print("lati : ${_position.latitude}");
-
-    FitnessCenter _fitness = await fitnessCenterRepo.getFitnessRepo(1, _position.longitude, _position.latitude, _position.longitude, _position.latitude);
+    print(_position);
+    FitnessCenter _fitness = await fitnessCenterRepo.getFitnessRepo(
+        1,
+        _position["user_longitude"],
+        _position["user_latitude"],
+        _position["user_longitude"],
+        _position["user_latitude"]);
     //FitnessCenter _fitness = await fitnessCenterRepo.getFitnessRepo(1, 100, 100, 100, 100);
-
+    print(_fitness);
+    print("hihihi");
     posts = _posts;
     banners = _banners;
     myFitnessCenter = _fitness;
+    print("START");
+    print("posts" +
+        _posts.toString() +
+        "banners" +
+        _banners.toString() +
+        "fitness_center" +
+        _fitness.toString());
+    print("END");
 
     return {"posts": _posts, "banners": _banners, "fitness_center": _fitness};
   }
