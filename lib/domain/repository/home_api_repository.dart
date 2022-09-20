ae4adab93d4aa5f2f8488f28248f739e7b8b58b7
bosoek <boseoggim08@gmail.com>
2022-09-20 11:34:39 +0900

    [FIX] profile button 범위 수정

--- lib/domain/repository/home_api_repository.dart
+++ lib/domain/repository/home_api_repository.dart
@@ -24,21 +24,21 @@ class HomeApiRepository {
     _banners = await bannerApi.getBanner();
     print("banner : $_banners");
 
-    Map _position = await locator.getAndSendLocation(null);
+    //Map _position = await locator.getAndSendLocation(null);
 
     //FitnessCenter _fitness = await fitnessCenterRepo.getFitnessRepo(1, _position.longitude, _position.latitude, _position.longitude, _position.latitude);
     // FitnessCenter _fitness =
     //     await fitnessCenterRepo.getFitnessRepo(1, 100, 100, 100, 100);
 
-    print(_position);
-
     FitnessCenter _fitness = await fitnessCenterRepo.getFitnessRepo(1, 126.76573544490464, 34.65468910081279, 128.67164341811144,  37.80553607680439);
     //FitnessCenter _fitness = await fitnessCenterRepo.getFitnessRepo(1, 100, 100, 100, 100);
     print(_fitness);
     print("hihihi");
+
     posts = _posts;
     banners = _banners;
     myFitnessCenter = _fitness;
+
     print("START");
     print("posts" +
         _posts.toString() +
